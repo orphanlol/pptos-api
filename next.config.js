f0563bf587b1f6e5198facb4670711d3e277697d
Dr. Quinten Van Damme <quintenvandamme@duck.com>
2023-03-12 11:43:00 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,18 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-async headers() {
-    return [
-      {
-        source: '/api/<route-name>',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, max-age=0',
-          },
-        ],
-      },
-    ];
-  },
-    
 module.exports = nextConfig
